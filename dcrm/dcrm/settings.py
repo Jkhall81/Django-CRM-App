836f547a9413c9f7e7ccf3e0d6043cf0aa2dcd0b
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-10 07:02:05 -0700

    fingers cross.

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -74,15 +74,18 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'db_iyy9',
+#        'USER': 'admin',
+#        'PASSWORD': 'qjN0VyOYEHyvWn0iYyRdRaC6m01defvj',
+#        'HOST': 'postgres://admin:qjN0VyOYEHyvWn0iYyRdRaC6m01defvj@dpg-ci27u067avj2t32u9brg-a/db_iyy9',
+#        'PORT': '5432',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_iyy9',
-        'USER': 'admin',
-        'PASSWORD': 'qjN0VyOYEHyvWn0iYyRdRaC6m01defvj',
-        'HOST': 'postgres://admin:qjN0VyOYEHyvWn0iYyRdRaC6m01defvj@dpg-ci27u067avj2t32u9brg-a/db_iyy9',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get('postgres://admin:qjN0VyOYEHyvWn0iYyRdRaC6m01defvj@dpg-ci27u067avj2t32u9brg-a/db_iyy9'), conn_max_age=600),
 }
 
 
