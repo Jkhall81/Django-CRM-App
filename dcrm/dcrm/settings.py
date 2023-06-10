f9b211a8351698e5b59189ee15d223e3c92863e7
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-10 07:08:03 -0700

    deploy try 2

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
-"""
 
+"""
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
